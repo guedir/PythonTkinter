21dd959423c19970113e1974c7e783f6ec65ef52
guedir <guedirmounir@gmail.com>
2024-03-04 11:02:04 +0100

    Première mise a jour

--- TP_Tkinter.py
+++ TP_Tkinter.py
@@ -5,6 +5,7 @@ fenetre = tkinter.Tk()
 fenetre.geometry("200x80")
 fenetre.resizable(False,False)
 fenetre.title("convertisseur")
+fenetre.configure(bg="#091821")
 
 convert = lambda nombre , taux : round(nombre*taux, 2)
 def convertir():
@@ -12,10 +13,10 @@ def convertir():
     resultat.config(text=str(convert(m,1.12)) +" Dollars US")
 
 montant = tkinter.DoubleVar()
-label = tkinter.Label(fenetre , text="Montant en euro?")
-zone_saisi = tkinter.Entry(fenetre , textvariable = montant)
-boutton = tkinter.Button(fenetre , text="Convertir" , command=convertir )
-resultat = tkinter.Message(fenetre , width=200)
+label = tkinter.Label(fenetre , text="Montant en euro?" , bg="#091821" , fg="white")
+zone_saisi = tkinter.Entry(fenetre , textvariable = montant , bd=3)
+boutton = tkinter.Button(fenetre , text="Convertir" , command=convertir , bd=2 ,bg="#FFA500" ,fg="White" )
+resultat = tkinter.Message(fenetre , width=200 , bg="#091821" , fg="white")
 
 label.grid(column=0 , row=0)
 zone_saisi.grid(column=0 , row=1 , padx=5)
