c6ac19f9a34c79368f65888ee4d96ca43e34b10b
guedir <guedirmounir@gmail.com>
2024-03-31 23:23:24 +0200

    modification du font

--- exercice.py
+++ exercice.py
@@ -13,11 +13,11 @@ def addition():
     resultat.config(text=f"{addi}")
 
 nombre = tkinter.IntVar()
-lbl1 = tkinter.Label(fenetre , text="Entrer N" , bg="#2F4F4F")
-zone_saisi = tkinter.Entry(fenetre , textvariable=nombre, bd=3 )
-lbl2 = tkinter.Label(fenetre , text="Le double de N =" , bg="#2F4F4F")
-resultat = tkinter.Label(fenetre , text="" , fg="red" , bg="#2F4F4F")
-btn = tkinter.Button(fenetre , text="Valider l'operation" , command=addition, bd=3)
+lbl1 = tkinter.Label(fenetre , text="Entrer N" , bg="#2F4F4F" , font=("sans serif",10,"bold"))
+zone_saisi = tkinter.Entry(fenetre , textvariable=nombre, bd=3 , font=("sans serif",10,"bold"))
+lbl2 = tkinter.Label(fenetre , text="Le double de N =" , bg="#2F4F4F", font=("sans serif",10,"bold"))
+resultat = tkinter.Label(fenetre , text="" , fg="red" , bg="#2F4F4F", font=("sans serif",10,"bold"))
+btn = tkinter.Button(fenetre , text="Valider l'operation" , command=addition, bd=3, font=("sans serif",10,"bold"))
 
 lbl1.grid(column=0 , row=0 , padx=25 , pady=25)
 zone_saisi.grid(column=1 , row=0 )
