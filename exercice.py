6878916a77b6eef1078f02bce67c9fb23997ba72
guedir <guedirmounir@gmail.com>
2024-04-12 11:26:06 +0200

    petite modification

--- exercice.py
+++ exercice.py
@@ -3,7 +3,7 @@ from tkinter import messagebox
 from tkinter import Tk
 
 fenetre = tkinter.Tk()
-fenetre.geometry("380x180+450+200")
+fenetre.geometry("450x180+450+200")
 fenetre.title("Addition")
 fenetre.resizable(True,False)
 fenetre.configure(bg="#2F4F4F")
@@ -30,15 +30,15 @@ lbl1 = tkinter.Label(fenetre , text="Entrer N" , bg="#2F4F4F" , font=("sans seri
 zone_saisi = tkinter.Entry(fenetre , textvariable=nombre, bd=3 , font=("sans serif",10,"bold"))
 lbl2 = tkinter.Label(fenetre , text="Le double de N =" , bg="#2F4F4F", font=("sans serif",10,"bold"))
 resultat = tkinter.Label(fenetre , text="" , fg="red" , bg="#2F4F4F", font=("sans serif",10,"bold"))
-btn = tkinter.Button(fenetre , text="Valider l'operation" , command=addition, bd=3, font=("sans serif",10,"bold"))
-btn2 = tkinter.Button(fenetre , text="Rénitialiser" , command=effacer,  bd=3, font=("sans serif",10,"bold"))
-btn3 = tkinter.Button(fenetre, text="Quitter", command=quitter,  bd=3, font=("sans serif",10,"bold") )
+btn = tkinter.Button(fenetre , text="VALIDER" , command=addition, bd=3, font=("sans serif",8,"bold"), width=10)
+btn2 = tkinter.Button(fenetre , text="RENITIALISER" , command=effacer,  bd=3, font=("sans serif",8,"bold"), width=10)
+btn3 = tkinter.Button(fenetre, text="QUITTER", command=quitter,  bd=3, font=("sans serif",8,"bold"), width=10 )
 
 lbl1.grid(column=0 , row=0 , padx=25 , pady=25)
 zone_saisi.grid(column=1 , row=0 )
 lbl2.grid(column=0 , row=1 , padx=25)
 resultat.grid(column=1 , row=1)
-btn.grid(column=0 , row=2 , pady=25)
+btn.grid(column=0 , row=2 , pady=25, padx=10 )
 btn2.grid(column=1 , row=2 , pady=25)
 btn3.grid(column=2 , row=2 , pady=25)
 
